b80282e0a3e7983a20282e4b4938d525e2b47ced
Karina Varughese <>
2025-03-03 11:27:31 -0600

    Update Website URL

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,7 +2,7 @@
 
 // Header section - Defines basic website information
 const header = {
-  homepage: 'https://karina-a-varughese.github.io/web-portfolio/', // URL for the homepage - !!! IMPORTANT: UPDATE THIS TO YOUR OFFICIAL WEBSITE LINK BEFORE DEPLOYING!!!!
+  homepage: 'https://karina-a-varughese.github.io/web-portfolio', // URL for the homepage - !!! IMPORTANT: UPDATE THIS TO YOUR OFFICIAL WEBSITE LINK BEFORE DEPLOYING!!!!
   title: 'GDSC', // Title shown in left of the navigation bar
 }
 
